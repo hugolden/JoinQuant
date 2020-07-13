1996fffa8a06c92590b24582b1d94d95f334c386
Sichao Hu <hsc89726@hotmail.com>
2020-07-13 23:08:34 +0800

    Add postgresql connection library

--- joinQuant/DataContainer.py
+++ joinQuant/DataContainer.py
@@ -1,7 +1,13 @@
+import jqdatasdk as sdk
+
 class DataContainer:
     def __init__(self):
         self.__stockList = []
 
+
+    def updateList(self):
+        self.__stockList = sdk.get_all_securities(types=['stock','fund'])
+        print(self.__stockList)
 
     def updateAllData(self):
         return False # update failed
--- joinQuant/MainModule.py
+++ joinQuant/MainModule.py
@@ -1,7 +1,14 @@
 import jqdatasdk as sdk
 import pandas as pd
 
+from joinQuant.DataContainer import DataContainer
 
 
-def bumpData():
+def auth():
     sdk.auth('18018517267','517267')
+
+
+if __name__ == '__main__':
+    auth()
+    container = DataContainer()
+    container.updateList()
