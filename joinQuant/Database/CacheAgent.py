a75e739dcab976f49e0028cd2bfbfcdee53ea628
Sichao Hu <hsc89726@hotmail.com>
2020-07-27 23:19:55 +0800

    Add cache concept

--- joinQuant/Database/CacheAgent.py
+++ joinQuant/Database/CacheAgent.py
@@ -1,3 +1,18 @@
 import json
 
-def readFromCache()
+def writeCompaniesToCache(stockList):
+    companiesDict = {}
+    companies = []
+    companiesDict['companies'] = companies
+    codes = stockList.index
+    for code, companySlice in codes:
+        companyFrame = {}
+        companyFrame['code'] = code
+        companyFrame['display_name']= companySlice['display_name']
+        companyFrame['name'] = companySlice['name']
+        companyFrame['start_date'] = companySlice['start_date']
+        companyFrame['end_date'] = companySlice['end_date']
+        companyFrame['type'] = companySlice['type']
+        companies.append(companyFrame)
+
+    serializedCompanies = json.dump(companiesDict)
